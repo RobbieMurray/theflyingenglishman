bdeca7f495f1ef28a5a474ec166ed046d3c57ddb
RobbieMurray <robbodym@gmail.com>
2021-05-31 18:30:41 -0700

    small changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `the flying englishman`,
+    description: `site for the flying englishman`,
+    author: `@theflyingenglishman`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
