d5e6169bf1eb93774f58c98795c3d4392bde6101
RobbieMurray <robbodym@gmail.com>
2021-06-07 00:14:09 -0700

    initial gear list with some amazon links

--- src/pages/gear.js
+++ src/pages/gear.js
@@ -1,14 +1,61 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import "../styles/global.css"
 
 const Gear = () => (
   <Layout>
     <Seo title="Gear" />
-    <h1>Gear list coming soon</h1>
-    <Link to="/">Go back to the homepage</Link>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textDecoration: "none",
+        color: "#2c2c2c",
+      }}
+    >
+      <h1>Gear List:</h1>
+      <ul>
+        <li>
+          <a href="https://amzn.to/3pzqkCv" alt="link to buy go pro hero 9">
+            Go Pro HERO 9
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/3ilg67h" alt="link to buy sd card">
+            Sandisk 256GB Micro SD Card
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/34TjNcf" alt="link to buy go pro media mod">
+            Go Pro HERO 9 Media Mod
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://amzn.to/34TjNcf"
+            alt="link to buy go pro el grande pole"
+          >
+            Go Pro El Grande Pole
+          </a>
+        </li>
+        <li>Polar Pro ND filters</li>
+        <li>Senheizer MKE 200 Shotgun Mic</li>
+        <li>
+          <a href="https://amzn.to/34W1gvS" alt="link to buy powerbank">
+            Mophie Powerstation Mini
+          </a>
+        </li>
+        <li>
+          <a href="https://amzn.to/34UGywI" alt="link to buy dji air 2s drone">
+            DJI Air 2s Fly More Combo
+          </a>
+        </li>
+      </ul>
+    </div>
   </Layout>
 )
 
