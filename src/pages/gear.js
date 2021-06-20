7aea89f7546b0eae22f56ac4d266e6deabbe83f6
RobbieMurray <robbodym@gmail.com>
2021-06-20 14:11:03 -0700

    Gear images added

--- src/pages/gear.js
+++ src/pages/gear.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import styled from "styled-components"
 import { device } from "../components/device"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Container = styled.div`
   display: flex;
@@ -21,6 +22,19 @@ const Container = styled.div`
 
   ul {
     padding: 1em;
+    margin: 0;
+  }
+
+  li {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    padding: 0.3em 0;
+  }
+
+  .gearimg {
+    width: 100px;
   }
 
   @media ${device.tablet} {
@@ -39,24 +53,61 @@ const Gear = () => (
       <h1>Gear List:</h1>
       <ul>
         <li>
+          <StaticImage
+            src="../images/gear/gopro.jpg"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="go pro hero 9"
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
           Go Pro HERO 9 |{" "}
           <a href="https://amzn.to/3pzqkCv" alt="link to buy go pro hero 9">
+            {" "}
             Buy
           </a>
         </li>
         <li>
+          <StaticImage
+            src="../images/gear/sdcard.jpg"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="sd card"
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
           Sandisk 256GB Micro SD Card |{" "}
           <a href="https://amzn.to/3ilg67h" alt="link to buy sd card">
             Buy
           </a>
         </li>
         <li>
+          <StaticImage
+            src="../images/gear/mediamod.jpg"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="media mod"
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
           Go Pro HERO 9 Media Mod |{" "}
           <a href="https://amzn.to/34TjNcf" alt="link to buy go pro media mod">
             Buy
           </a>
         </li>
         <li>
+          <StaticImage
+            src="../images/gear/elgrande.jpg"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt=""
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
           Go Pro El Grande Pole |{" "}
           <a
             href="https://amzn.to/2TzEpnp"
@@ -66,7 +117,16 @@ const Gear = () => (
           </a>
         </li>
         <li>
-          Polar Pro ND filters for Go Pro HERO 9 |{" "}
+          <StaticImage
+            src="../images/gear/polarpro.png"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="the flying englishman filming"
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
+          Polar Pro ND Filters |{" "}
           <a
             href="https://www.polarprofilters.com/collections/filters/products/hero9-nd-filters"
             alt="link to buy polar pro nd filters for gopro hero 9"
@@ -75,7 +135,16 @@ const Gear = () => (
           </a>
         </li>
         <li>
-          Sennheiser MKE 200 Shotgun Mic |{" "}
+          <StaticImage
+            src="../images/gear/mke200.jpg"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="the flying englishman filming"
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
+          Sennheiser MKE 200 Microphone |{" "}
           <a
             href="https://amzn.to/3xpT9DY"
             alt="link to buy Sennheiser MKE 200 Shotgun microphone"
@@ -84,12 +153,30 @@ const Gear = () => (
           </a>
         </li>
         <li>
+          <StaticImage
+            src="../images/gear/batterypack.jpg"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="batterypack"
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
           Mophie Powerstation Mini |{" "}
           <a href="https://amzn.to/34W1gvS" alt="link to buy powerbank">
             Buy
           </a>
         </li>
         <li>
+          <StaticImage
+            src="../images/gear/drone.jpg"
+            quality={95}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="dji air 2s drone"
+            layout="constrained"
+            margin="150px"
+            className="gearimg"
+          />
           DJI Air 2s Fly More Combo |{" "}
           <a href="https://amzn.to/34UGywI" alt="link to buy dji air 2s drone">
             Buy
