f8ffbe81edcf5afee2f763230d0e8c124e8bcc7e
RobbieMurray <robbodym@gmail.com>
2023-11-14 22:29:11 +0000

    h264 video

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/SEO";
 import BannerModule from "../components/BannerModule/BannerModule";
-import videoHero from "../videos/hero.webm";
+import videoHero from "../videos/hero.mp4";
 
 const Index = () => {
   return (
